341ac77cef7c24e3893faf176f545d5fcfb1f619
smellystank <ryan.peh.wye.kin.9795@gmail.com>
2023-06-11 17:51:20 +0800

    Profile Picture Implemented
    - Firebase usage
    
    Further Actions:
    1. To use authstate to ref profile pic
    2. use db concepts to try implementing chat/forum

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Optionally import the services that you want to use
 // import {...} from "firebase/auth";
-// import {...} from "firebase/database";
 // import {...} from "firebase/firestore";
 // import {...} from "firebase/functions";
 // import {...} from "firebase/storage";
 
 // Initialize Firebase
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBOWm04rFb11LP7ct4EZTvGFT7TZpoOYFU",
   authDomain: "polaris-7faa8.firebaseapp.com",
   projectId: "polaris-7faa8",
@@ -20,6 +20,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const db = getFirestore(app);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
