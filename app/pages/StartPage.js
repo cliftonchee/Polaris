602dc63033af0cfc21a48915b4ea23434a290420
Clifton <yeoclifton@gmail.com>
2023-06-18 00:20:25 -0700

    Integration for iOS in progress

--- app/pages/StartPage.js
+++ app/pages/StartPage.js
@@ -1,67 +1,100 @@
-import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
-import colours from "../components/Colours";
-import Button from "../components/Button";
+// import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+// import colours from "../components/Colours";
+// import Button from "../components/Button";
 
-// To navigate between screens, add '{ navigation }'
-// as an argument, and under onPress, refer to below
-export default function StartPage({ navigation }) {
+// // To navigate between screens, add '{ navigation }'
+// // as an argument, and under onPress, refer to below
+// export default function StartPage({ navigation }) {
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Image
+//         style={styles.image}
+//         source={require("../assets/images/polaris-constellation.png")}
+//       />
+
+//       {/* Title */}
+//       <Text style={styles.title}>P O L A R I S</Text>
+//       {/* Subtitle */}
+//       <Text style={styles.subtitle}>Connect with the stars</Text>
+
+//       {/* Customisable Button */}
+//       <View style={styles.button}>
+//         <Button
+//           styleOverride={styles.buttonLayout}
+//           // Navigation to Login page
+//           onPress={() => navigation.navigate("Login")}
+//           title="Get started"
+//         ></Button>
+//       </View>
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     // Background
+//     flex: 1,
+//     alignItems: "center",
+//     backgroundColor: colours.primary,
+//   },
+//   image: {
+//     flex: 3,
+//     justifyContent: "center",
+//     maxWidth: 960,
+//   },
+//   title: {
+//     flex: 0.5,
+//     alignItems: "center",
+//     fontSize: 50,
+//     color: colours.white,
+//     fontFamily: "LatoBold",
+//   },
+//   subtitle: {
+//     flex: 0.4,
+//     fontSize: 24,
+//     color: colours.subtitle,
+//     fontFamily: "LatoRegular",
+//   },
+//   buttonLayout: {
+//     // Added on top of original button
+//     borderRadius: 30,
+//     width: 350,
+//     height: 100,
+//   },
+//   button: {
+//     flex: 1,
+//   },
+// });
+
+import React, { useRef, useEffect } from 'react';
+import UnityView from '@azesmway/react-native-unity';
+import { View } from 'react-native';
+
+const Unity = () => {
+  const unityRef = useRef(null);
+
+  useEffect(() => {
+    if (unityRef.current) {
+      const message = {
+        gameObject: 'gameObject',
+        methodName: 'methodName',
+        message: 'message',
+      };
+      unityRef.current.postMessage(message.gameObject, message.methodName, message.message);
+    }
+  }, []);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Image
-        style={styles.image}
-        source={require("../assets/images/polaris-constellation.png")}
+    <View style={{ flex: 1 }}>
+      <UnityView
+        ref={unityRef}
+        style={{ flex: 1 }}
+        onUnityMessage={(result) => {
+          console.log('onUnityMessage', result.nativeEvent.message);
+        }}
       />
-
-      {/* Title */}
-      <Text style={styles.title}>P O L A R I S</Text>
-      {/* Subtitle */}
-      <Text style={styles.subtitle}>Connect with the stars</Text>
-
-      {/* Customisable Button */}
-      <View style={styles.button}>
-        <Button
-          styleOverride={styles.buttonLayout}
-          // Navigation to Login page
-          onPress={() => navigation.navigate("Login")}
-          title="Get started"
-        ></Button>
-      </View>
-    </SafeAreaView>
+    </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    // Background
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: colours.primary,
-  },
-  image: {
-    flex: 3,
-    justifyContent: "center",
-    maxWidth: 960,
-  },
-  title: {
-    flex: 0.5,
-    alignItems: "center",
-    fontSize: 50,
-    color: colours.white,
-    fontFamily: "LatoBold",
-  },
-  subtitle: {
-    flex: 0.4,
-    fontSize: 24,
-    color: colours.subtitle,
-    fontFamily: "LatoRegular",
-  },
-  buttonLayout: {
-    // Added on top of original button
-    borderRadius: 30,
-    width: 350,
-    height: 100,
-  },
-  button: {
-    flex: 1,
-  },
-});
+export default Unity;
